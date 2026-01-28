72a9cbf9fb64d9679313bd608f802001dce61b43
Leberns <lebernsmuller@gmail.com>
2026-01-28 12:12:46 +0100

    check ai connection

---
+++ src/tools/CheckChatAzure/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenAI.Chat;
+using Azure.Identity;
+using Azure.AI.OpenAI;
+
+var endpoint = new Uri("https://cog-wiok5xjx2aqja.openai.azure.com/");
+var deploymentName = "chat-model";
+
+AzureOpenAIClient azureClient = new(
+    endpoint,
+    new DefaultAzureCredential());
+
+ChatClient chatClient = azureClient.GetChatClient(deploymentName);
+
+var requestOptions = new ChatCompletionOptions()
+{
+    MaxOutputTokenCount = 4096,
+    Temperature = 1.0f,
+    TopP = 1.0f,
+
+};
+
+List<ChatMessage> messages = new List<ChatMessage>()
+{
+    new SystemChatMessage("You are a helpful assistant."),
+    new UserChatMessage("I am going to Paris, what should I see?"),
+};
+
+var response = chatClient.CompleteChat(messages, requestOptions);
+System.Console.WriteLine(response.Value.Content[0].Text);
